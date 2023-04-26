0ddb20b7d645d086c40eaed90b68ddad3043202c
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-26 17:23:43 +0530

    Added: Maximum more than 3 parameters

---
+++ MaxMorethan3Parameter.java
@@ -0,0 +1,22 @@
+package com.java;
+import java.util.*;
+public class MaxMorethan3Parameter <T extends Comparable<T>>{
+	public static <T extends Comparable<T>> T findMax(List<T> arr) {
+
+		T max = arr.get(0);
+		for (T key : arr) {
+			if (key.compareTo(max) > 0)
+				max = key;
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		MaxMorethan3Parameter obj = new MaxMorethan3Parameter();
+		System.out.println("The Maximum integer num is:" + obj.findMax(Arrays.asList(4, 7, 8, 9, 44, 55)));
+		System.out.println("The Maximum float num is:" + obj.findMax(Arrays.asList(1.9, 3.0, 6.7, 2.6, 4.5, 5.8)));
+		System.out.println("The Maximum String  is:" + obj.findMax(Arrays.asList("Apple", "banana", "peach", "mango", "kiwi")));
+
+	}
+
+}
